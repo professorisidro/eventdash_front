a0ada986f82ad94b41326503a503a9a02005fa8f
professorisidro <isidro@professorisidro.com.br>
2021-08-19 16:38:32 -0300

    Relatorio de eventos

--- relatorios.js
+++ relatorios.js
@@ -10,4 +10,15 @@ function carregaInfo(){
     var user = JSON.parse(userSTR);  // como eu gravei em fomato texto, vou reconverter para objeto em memória
 
     document.getElementById("fotoUser").innerHTML = `<img src="${user.linkFoto}"  width="100%">`;
+    document.getElementById("bioUser").innerHTML = `<h4> ${user.nome} </h4>
+                                                    <hr>
+                                                    <b> RACF: </b> ${user.racf} <br>
+                                                    <b> Email: </b> ${user.email} <br>
+                                                    <b> Departamento: </B> ${user.departamento} <br>
+                                                    <button type="button" class="btn btn-primary" onclick="logout()"> Logout </button>`;
+}
+
+function logout(){
+    localStorage.removeItem("userDASH");
+    window.location = "index.html";
 }
