18a97a04584fdb0b1bf1b2f8f4065eaddc86e40b
professorisidro <isidro@professorisidro.com.br>
2021-08-19 17:22:57 -0300

    Relatorio de eventos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,22 @@
 function gerarRelatorio(){
-    
+    var dtIni = document.getElementById("txtDataIni").value;
+    var dtFim = document.getElementById("txtDataFim").value;
+
+    var url = "http://localhost:8080/eventos?ini="+dtIni+"&fim="+dtFim;
+
+    fetch(url).then(res => res.json()).then(listaEventos => preencheTabela(listaEventos));
+
+    /* se eu fosse ler como uma instrução estruturada
+
+    res = fetch(url);
+    listaEventos = res.json();
+    preencheTabela(listaEventos);
+
+    */
+
+}
+
+function preencheTabela(listaEventos){
+    console.log(listaEventos);
+
 }
