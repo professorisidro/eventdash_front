15540ce98d7d229830847b691e58e4a038ccd34a
professorisidro <isidro@professorisidro.com.br>
2021-08-19 17:49:44 -0300

    Relatorio de eventos

--- eventos.js
+++ eventos.js
@@ -18,5 +18,34 @@ function gerarRelatorio(){
 
 function preencheTabela(listaEventos){
     console.log(listaEventos);
+    var strTabela = `<table class="table">
+                        <thead>
+                            <th class="text-center"> numSeq </th>
+                            <th class="text-center"> data </th>
+                            <th class="text-center"> Alarme </th>
+                            <th class="text-center"> Hostname </th>
+                            <th class="text-center"> IP </th>
+                        </thead> 
+                        
+                        <tbody> `;
+
+    // agora eu preciso percorrer a lista e preencher cada uma das linhas
+    for (i=0; i<listaEventos.length; i++){
+        let evento = listaEventos[i];
+
+        strTabela = strTabela + `<tr>
+                                     <td> ${evento.numSeq} </td>
+                                     <td> ${evento.dataEvento} </td>
+                                     <td> ${evento.alarme.nome} </td>
+                                     <td> ${evento.equipamento.hostname} </td>
+                                     <td> ${evento.equipamento.ipaddr} </td>
+                                 </tr>`; 
+
+    }
+
+    strTabela = strTabela + `   </tbody>
+                             </table>`;
+    
+    document.getElementById("relatorio").innerHTML = strTabela;
 
 }
